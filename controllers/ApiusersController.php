4ee95f63e2d5c2b43214dce6f10c49fbe76bcd81
Ketan Sangani <ketansangani12@gmail.com>
2020-10-05 16:01:43 +0530

    resolved sms otp

--- controllers/ApiusersController.php
+++ controllers/ApiusersController.php
@@ -341,7 +341,7 @@ class ApiusersController extends ActiveController
                     $curl = curl_init();
 
                     curl_setopt_array($curl, array(
-                        CURLOPT_URL => "https://secure.etracker.cc/MobileOTPAPI/SMSOTP/OTPGenerate?user=TEST177&from=RUMAH&servid=MES01&ApiReturnType=2&pass=SyR%2526PbN0&text=Your%20Rumah-i%20pincode%20is%20%3COTPCode%3E.&to=".$contact_no."&type=0",
+                        CURLOPT_URL => "https://secure.etracker.cc/MobileOTPAPI/SMSOTP/OTPGenerate?user=TEST177&pass=SyR%26PbN0&from=RUMAH&servid=MES01&ApiReturnType=2&text=Your%20Rumah-i%20pincode%20is%20%3COTPCode%3E.&to=".$contact_no."&type=0",
                         CURLOPT_RETURNTRANSFER => true,
                         CURLOPT_ENCODING => "",
                         CURLOPT_MAXREDIRS => 10,
