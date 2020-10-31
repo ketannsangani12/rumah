f56cbcbbfc0dac1d48d2069cb0ddab204abe3e75
Ketan Sangani <ketansangani12@gmail.com>
2020-10-31 12:59:15 +0530

    agent name in todo detail

--- controllers/ApiusersController.php
+++ controllers/ApiusersController.php
@@ -2074,7 +2074,7 @@ class ApiusersController extends ActiveController
         } else {
             $user_id = $this->user_id;
            // echo $user_id;exit;
-            $todolists = TodoList::find()->select(['id','title','description','reftype','status','request_id','renovation_quote_id','service_request_id','property_id','user_id','landlord_id','agent_id','vendor_id','worker_id','created_at','updated_at','rent_startdate','rent_enddate','pay_from','service_type','due_date','appointment_date','appointment_time','subtotal','sst','total', new \yii\db\Expression("CONCAT('/uploads/tododocuments/', '', `document`) as document")])
+            $todolists = TodoList::find()->select(['id','title','description','reftype','status','request_id','renovation_quote_id','service_request_id','property_id','user_id','landlord_id','agent_id','vendor_id','worker_id','created_at','updated_at','rent_startdate','rent_enddate','pay_from','service_type','due_date','appointment_date','appointment_time','subtotal','sst','total', new \yii\db\Expression("CONCAT('/uploads/tododocuments/', '', `document`) as document"),"commission"])
                 ->with([
                     'request'=>function ($query) {
                         $query->select(['id','booking_fees','credit_score','monthly_rental','tenancy_fees','stamp_duty','keycard_deposit','rental_deposit','utilities_deposit','subtotal','total','commencement_date','tenancy_period','security_deposit','status',new \yii\db\Expression("CONCAT('/uploads/creditscorereports/', '', `credit_score_report`) as credit_score_report"),new \yii\db\Expression("CONCAT('/uploads/agreements/', '', `agreement_document`) as agreement_document"),new \yii\db\Expression("CONCAT('/uploads/moveinout/', '', `movein_document`) as movein_document"),new \yii\db\Expression("CONCAT('/uploads/moveinout/', '', `moveout_document`) as moveout_document")]);
@@ -2088,15 +2088,15 @@ class ApiusersController extends ActiveController
                     'property'=>function($query){
                         $query->select('id,property_no,title');
                     },
-                    'user'=>function($query){
-                        $query->select("id,full_name");
+                    'user' => function ($query) {
+                        $query->select(["id","full_name",new \yii\db\Expression("CONCAT('/uploads/users/', '', `image`) as profile_picture")]);
                     },
-                    'landlord'=>function($query){
-                        $query->select("id,full_name");
+                    'landlord' => function ($query) {
+                        $query->select(["id","full_name",new \yii\db\Expression("CONCAT('/uploads/users/', '', `image`) as profile_picture")]);
 
                     },
                     'agent'=>function($query){
-                        $query->select("id,full_name,company_name");
+                        $query->select(["id","full_name","company_name",new \yii\db\Expression("CONCAT('/uploads/users/', '', `image`) as profile_picture")]);
 
                     },
                     'renovationquote'=>function($query){
