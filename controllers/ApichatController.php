6d74f26c735c12c9e7c6927b6b978c27c949c862
Ketan Sangani <ketansangani12@gmail.com>
2021-02-17 17:18:27 +0530

    chat fix

--- controllers/ApichatController.php
+++ controllers/ApichatController.php
@@ -121,8 +121,7 @@ class ApichatController extends ActiveController
             $query = Chats::find()->select(['case when (sender_id = '.$this->userId.') then receiver_id else sender_id end as opponent_id','rumah_chats.id','msg','msg_type',
                 '(select max(rc2.created_at) from rumah_chats as rc2 where (rc2.sender_id = rumah_chats.sender_id and rc2.receiver_id = rumah_chats.receiver_id) or (rc2.sender_id = rumah_chats.receiver_id and rc2.receiver_id = rumah_chats.sender_id)) as created_at','rumah_chats.property_id','rumah_chats.sender_id','rumah_chats.receiver_id'])
                 ->orderBy([
-                    'rumah_chats.id' => SORT_DESC,
-                    'rumah_chats.created_at' => SORT_DESC
+                    'created_at' => SORT_DESC
                 ])
                 ->joinWith(['sender'=>function($q) use ($baseurl){
                     $q->select(['id','full_name','role','case when rumah_users.image != "" then CONCAT("'.$baseurl.'/uploads/users/",rumah_users.image) else "" end as image']);
