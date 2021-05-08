5a4ffcfe3258cf7453512298d5fdcb9e3d70616e
Ketan Sangani <ketansangani12@gmail.com>
2021-05-08 16:06:28 +0530

    change in property detail api

--- controllers/ApiusersController.php
+++ controllers/ApiusersController.php
@@ -2025,6 +2025,18 @@ class ApiusersController extends ActiveController
                         $agentratings = $ratings/$totalratings;
                     }
                 }
+                $bookingrequestmodel = BookingRequests::find()->select('id,status')->where(['property_id'=>$property_id])->one();
+                if(!empty($bookingrequestmodel)){
+                    if($bookingrequestmodel->status=='Approved' || $bookingrequestmodel->status=='Processing' || $bookingrequestmodel->status=='Processed' || $bookingrequestmodel->status=='Agreement Processing' || $bookingrequestmodel->status=='Payment Requested' || $bookingrequestmodel->status=='Rented'){
+                        $bookingexists = 1;
+                    }else{
+                        $bookingexists = 0;
+                    }
+
+                }else{
+                    $bookingexists = 0;
+                }
+                $propertydata['bookingexists'] = $bookingexists;
                 $data['agentratings'] = number_format((float)$agentratings, 2, '.', '');
                 return array('status' => 1, 'data' => $data);
 
