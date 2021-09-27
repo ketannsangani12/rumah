66ea2735b2119067246b1f15b7ee2bc82112f139
Ketan Sangani <ketansangani12@gmail.com>
2021-09-27 17:09:58 +0530

    bug fixing for ekyc document

--- controllers/ApiusersController.php
+++ controllers/ApiusersController.php
@@ -3281,12 +3281,18 @@ class ApiusersController extends ActiveController
                     switch ($todolist['reftype']){
                         case "Booking";
                             if($todolist['user_id']==$user_id){
-
                                 if(($todolist['status']=='Pending' && $todolist['request']['credit_score']=='') || $todolist['status']=='New' || $todolist['status']=='Approved' || $todolist['status']=='Unpaid'){
-
                                     if($todolist['status']=='Approved'){
                                         if($todolist['user']['identity_status']=='Verified'){
-
+                                            if($todolist['user']['document_front']!=''){
+                                                $todolist['user']['ekyc_document'] = $todolist['user']['document_front'];
+                                            }else if($todolist['user']['document_back']!=''){
+                                                $todolist['user']['ekyc_document_back'] = $todolist['user']['document_back'];
+                                            }else if($todolist['user']['ekyc_document']!=''){
+                                                $todolist['user']['document_front'] = $todolist['user']['ekyc_document'];
+                                            }else if($todolist['user']['ekyc_document_back']!=''){
+                                                $todolist['user']['document_back'] = $todolist['user']['ekyc_document_back'];
+                                            }
                                             $data[] = $todolist;
                                         }else{
                                             $manualkycexist = ManualKyc::find()->where(['user_id'=>$user_id,'request_id'=>$todolist['request']['id'],'status'=>'Pending'])->one();
@@ -3305,22 +3311,8 @@ class ApiusersController extends ActiveController
                                         $data[] = $todolist;
                                     }
                                 }
-                                if($todolist['user']['identity_status']=='Verified') {
-                                    if ($todolist['user']['document_front'] != '') {
-                                        $todolist['user']['ekyc_document'] = $todolist['user']['document_front'];
-                                    }
-                                    if ($todolist['user']['document_back'] != '') {
-                                        $todolist['user']['ekyc_document_back'] = $todolist['user']['document_back'];
-                                    }  if ($todolist['user']['ekyc_document'] != '') {
-                                        $todolist['user']['document_front'] = $todolist['user']['ekyc_document'];
-                                    }  if ($todolist['user']['ekyc_document_back'] != '') {
-                                        $todolist['user']['document_back'] = $todolist['user']['ekyc_document_back'];
-                                    }
-                                    $data[] = $todolist;
-                                }
 
                             }else if($todolist['landlord_id']==$user_id){
-
                                 if(($todolist['status']=='Pending' && $todolist['request']['credit_score']!='') ||  $todolist['status']=='Processing' || $todolist['request']['status']=='Agreement Processing'){
                                     if($todolist['status']=='Processing'){
                                         if($todolist['landlord']['identity_status']=='Verified'){
@@ -3335,17 +3327,6 @@ class ApiusersController extends ActiveController
                                     }else {
                                         $data[] = $todolist;
                                     }
-                                }
-                                if($todolist['landlord']['identity_status']=='Verified'){
-
-                                     if($todolist['landlord']['ekyc_document']!=''){
-                                        $todolist['landlord']['document_front'] = $todolist['landlord']['ekyc_document'];
-                                    }
-                                    if($todolist['landlord']['ekyc_document_back']!=''){
-                                        $todolist['landlord']['document_back'] = $todolist['landlord']['ekyc_document_back'];
-                                    }
-
-                                    $data[] = $todolist;
                                 }
                             }
 
