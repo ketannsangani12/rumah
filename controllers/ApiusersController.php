cc160909dad16c8441174752e7e519d8c8ea850c
Ketan Sangani <ketansangani12@gmail.com>
2021-09-27 16:32:09 +0530

    bug fixing for ekyc document

--- controllers/ApiusersController.php
+++ controllers/ApiusersController.php
@@ -3284,6 +3284,15 @@ class ApiusersController extends ActiveController
                                 if(($todolist['status']=='Pending' && $todolist['request']['credit_score']=='') || $todolist['status']=='New' || $todolist['status']=='Approved' || $todolist['status']=='Unpaid'){
                                     if($todolist['status']=='Approved'){
                                         if($todolist['user']['identity_status']=='Verified'){
+                                            if($todolist['user']['document_front']!=''){
+                                                $todolist['user']['ekyc_document'] = $todolist['user']['document_front'];
+                                            }else if($todolist['user']['document_back']!=''){
+                                                $todolist['user']['ekyc_document_back'] = $todolist['user']['document_back'];
+                                            }else if($todolist['user']['ekyc_document']!=''){
+                                                $todolist['user']['document_front'] = $todolist['user']['ekyc_document'];
+                                            }else if($todolist['user']['ekyc_document_back']!=''){
+                                                $todolist['user']['document_back'] = $todolist['user']['ekyc_document_back'];
+                                            }
                                             $data[] = $todolist;
                                         }else{
                                             $manualkycexist = ManualKyc::find()->where(['user_id'=>$user_id,'request_id'=>$todolist['request']['id'],'status'=>'Pending'])->one();
@@ -3308,6 +3317,15 @@ class ApiusersController extends ActiveController
                                     if($todolist['status']=='Processing'){
                                         if($todolist['landlord']['identity_status']=='Verified'){
                                             $data[] = $todolist;
+                                            if($todolist['landlord']['document_front']!=''){
+                                                $todolist['landlord']['ekyc_document'] = $todolist['landlord']['document_front'];
+                                            }else if($todolist['landlord']['document_back']!=''){
+                                                $todolist['landlord']['ekyc_document_back'] = $todolist['landlord']['document_back'];
+                                            }else if($todolist['landlord']['ekyc_document']!=''){
+                                                $todolist['landlord']['document_front'] = $todolist['landlord']['ekyc_document'];
+                                            }else if($todolist['landlord']['ekyc_document_back']!=''){
+                                                $todolist['landlord']['document_back'] = $todolist['landlord']['ekyc_document_back'];
+                                            }
                                         }else{
                                             $manualkycexist = ManualKyc::find()->where(['user_id'=>$user_id,'request_id'=>$todolist['request']['id'],'status'=>'Pending'])->one();
                                             if(empty($manualkycexist)){
